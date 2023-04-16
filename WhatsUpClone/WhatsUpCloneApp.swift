c108af24bb38315a130903545c078b0e5222e3e6
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-04-16 14:28:47 +0300

    Initial Commit

---
+++ WhatsUpClone/WhatsUpCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpCloneApp.swift
+//  WhatsUpClone
+//
+//  Created by YILDIRIM on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
